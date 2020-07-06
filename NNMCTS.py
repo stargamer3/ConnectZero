2ef86991b1bd8472483d1e6ea56d57dcf6e9b7af
k3w1k0d3r <juliengrijalva@gmail.com>
2020-07-06 11:15:04 -0600

    Update NNMCTS.py

--- NNMCTS.py
+++ NNMCTS.py
@@ -10,13 +10,17 @@ class state():
         self.getboard = game.getboard
         self.moveset = game.moveset
         self.movelegal = game.movelegal
+        self.players = game.players
         self.game = game
         self.parent = parent
         self.c = c
         self.P = 0
         self.Q = 0
-        self.W = [0, 0]
-        self.V = [0, 0]
+        self.W = {}
+        self.V = {}
+        for i in self.players:
+            self.W[i] = 0
+            self.V[i] = 0
         self.children = []
         self.move = move
     def new_child(self, move, P):
@@ -36,13 +40,20 @@ class state():
             if(self.movelegal(self.board, i)):
                 unexplored.append(i)
         return True, unexplored
-    def backprop(self, W=[0, 0]):
+    def backprop(self, W=None):
         self.visits+=1
+        if(W is None):
+            W = {}
+            for i in self.players:
+                W[i] = 0
         if(self.parent is not None):
-            self.W[0]+=W[0]
-            self.W[1]+=W[1]
-            self.Q = (self.W[(self.parent.player+1)//2]+self.V[(self.parent.player+1)//2])/self.visits
-            return self.parent.backprop([self.W[0]+self.V[0], self.W[1]+self.V[1]])
+            for i in players:
+                self.W[i]+=W[i]
+            self.Q = (self.W[self.parent.player]+self.V[self.parent.player])/self.visits
+            bpW = {}
+            for i in players:
+                bpW[i] = self.W[i]+self.V[i]
+            return self.parent.backprop(bpW)
         return self
     def get_score(self):
         if(self.parent is not None):
